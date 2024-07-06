a7a8e746d82fd2fa651529a7916259c2a9c5dedb
Ryan <rdebusk97@gmail.com>
2024-07-06 16:29:14 -0500

    Update README

--- src/hooks/use-settings.js
+++ src/hooks/use-settings.js
@@ -1,18 +1,11 @@
 import { useState } from 'react';
 
 function useSettings() {
-
-    // Load the initial state from local storage
     const initialRandomized = JSON.parse(localStorage.getItem('randomized')) || false;
     const initialAutoPlay = JSON.parse(localStorage.getItem('autoplay')) || false;
 
-    //const initialUser = JSON.parse(localStorage.getItem('user')) || "";
-    //const initialPass = JSON.parse(localStorage.getItem('pass')) || "";
-
     const [randomized, setRandomized] = useState(initialRandomized);
     const [autoPlay, setAutoPlay] = useState(initialAutoPlay);
-    //const [user, setUser] = useState(initialUser);
-    //const [pass, setPass] = useState(initialPass);
 
     const updateRandomized = () => { 
         localStorage.setItem('randomized', JSON.stringify(!randomized));
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,5 +1,5 @@
 import '../styles/SettingsPage.css';
-import { Switch, FormControlLabel, Typography, TextField } from '@mui/material';
+import { Switch, FormControlLabel, Typography } from '@mui/material';
 import useSettings from '../hooks/use-settings';
 
 function SettingsPage() {
@@ -12,11 +12,6 @@ function SettingsPage() {
                 <Typography>Song Guessing Options</Typography>
                 <FormControlLabel control={<Switch checked={randomized} onChange={(e) => updateRandomized(e.target.checked)} />} label="Randomize Songs" />
                 <FormControlLabel control={<Switch checked={autoPlay} onChange={(e) => updateAutoPlay(e.target.checked)} />} label="Auto Play Songs" />
-            </div>
-            <div className="section">
-                {/*<Typography>Spotify Credentials</Typography>
-                <FormControlLabel control={<TextField sx={{padding: '5px'}} size="small"/>} label="User"/>
-                <FormControlLabel control={<TextField sx={{padding: '5px'}} size="small"/>} label="Pass"/>*/}
             </div>
         </div>
     );
